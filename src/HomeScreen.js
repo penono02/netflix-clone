2cce7e46c99377e9e8434fa15c49df22c3ca6235
Sainval <Edneau.Sainval@KofC.org>
2022-11-08 23:17:34 -0500

    first commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Nav from "./Nav";
+import "./HomeScreen.css";
+
+function HomeScreen() {
+  return (
+    <div className="HomeScreen">
+      {/*NavBar */}
+      <Nav />
+
+      {/* Banner */}
+
+      {/*Rows */}
+    </div>
+  );
+}
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBuv7T_mxS79uLuxe_oMlfR18r4zKI9yY",
+  authDomain: "nextflix-clone-7d60a.firebaseapp.com",
+  projectId: "nextflix-clone-7d60a",
+  storageBucket: "nextflix-clone-7d60a.appspot.com",
+  messagingSenderId: "7392015312",
+  appId: "1:7392015312:web:dda554575b8d0b46d87fd5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
